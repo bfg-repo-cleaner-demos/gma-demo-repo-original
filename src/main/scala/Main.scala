0a73b44f37b89568cc0569fef0f55c72c1fd7a17
Roberto Tyley <roberto.tyley@gmail.com>
2013-04-03 20:28:32 +0100

    Revert "Carelessly checking password into source control"
    This reverts commit 8c9cfe3c287af12580fceda09c22df7aec72c1fa.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,3 @@
-import java.util.Properties
-
 object HelloWorld extends App {
   println("Hello, world!")
-
-  val properties = new Properties()
-  properties.load(getClass.getResource("config.properties").openStream())
-  println(s"Gosh, we're using this password: ${properties.getProperty("db.password")}")
 }
