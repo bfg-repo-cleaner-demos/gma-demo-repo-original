75b24d0eaf57008aaa2f98f3dd24bdb31fdfa1ed
Roberto Tyley <roberto.tyley@gmail.com>
2013-04-03 20:39:13 +0100

    Add factorials, because we all love factorials

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
 import java.util.Properties
 
 object HelloWorld extends App {
+
+  def fact(n: Int): BigInt = if (n == 0) 1 else (n * fact(n-1))
+
   println("Hello, world!")
 
   val properties = new Properties()
   properties.load(getClass.getResource("config.properties").openStream())
   println(s"Gosh, we're using this password: ${properties.getProperty("db.password")}")
+
+  println(s"I like factorials ${fact(10)}")
 }
