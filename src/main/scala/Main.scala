2213243bc72bed3bd0a608d5331607d33553fe8d
Roberto Tyley <roberto.tyley@gmail.com>
2013-04-03 20:32:24 +0100

    Check in config code again, this time with password removed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,9 @@
+import java.util.Properties
+
 object HelloWorld extends App {
   println("Hello, world!")
+
+  val properties = new Properties()
+  properties.load(getClass.getResource("config.properties").openStream())
+  println(s"Gosh, we're using this password: ${properties.getProperty("db.password")}")
 }
